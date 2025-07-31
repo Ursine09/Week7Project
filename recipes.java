180ccd3e914693b8a2edd98cc5185dda18c8398e
GitUser <120816634+Ursine09@users.noreply.github.com>
2025-07-31 00:36:08 -0400

    Update recipes.java

--- recipes.java
+++ recipes.java
@@ -1,29 +1,20 @@
-package recipes.exception;
+package projects.exception;
 
-public class recipes extends RuntimeException {
 
-	public recipes() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public recipes(String message) {
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+	
+	public DbException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
-
-	public recipes(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public recipes(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public recipes(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	public DbException(Throwable cause) {
+	super(cause);
+}
+	
+	public DbException(String message, Throwable cause) {
+	super(message, cause);
+	
+}
 }
